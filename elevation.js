5fb9ce0153081e10ab0a63752b58d5f31dec2f00
Michael S. P. Kelley <msk@astro.umd.edu>
2016-09-15 21:47:15 -0400

    Working on adding the Sun.

--- elevation.js
+++ elevation.js
@@ -1,16 +1,65 @@
-// http://vo.imcce.fr/webservices/miriade/ephemcc_query.php?-name=c:2P&-ep=2016-09-14&-from:maryland&-mime=html
 $(document).ready(
   function() {
     $('#plot-button').click(plotData);
+    $('#plot-button').click();
   }
 );
 
 var layout = {
-  xaxis: {title: 'Civil Time (hr)'},
-  yaxis: {title: 'Elevation (deg)'},
-  margin: {t: 20},
+  xaxis: {title: 'Civil Time (hr)', range: [-7, 7]},
+  yaxis: {title: 'Elevation (deg)', range: [0, 90]},
+  margin: {
+    t: 10,
+    b: 50,
+    l: 50,
+    r: 50,
+  },
   hovermode: 'closest'
 };
+
+function getIMCCE(name, type, date, done) {
+  // http://vo.imcce.fr/webservices/miriade/ephemcc_query.php?-name=c:2P&-ep=2016-09-14&-from:maryland&-mime=html
+  data = {};
+  data['-name'] = type + ':' + name;
+  data['-ep'] = date;
+  data['-mime'] = 'text';
+  data['-from'] = 'elevation-webapp';
+  $.get('http://vo.imcce.fr/webservices/miriade/ephemcc_query.php', data)
+    .done(done);
+}
+
+function processIMCCE(data) {
+  console.log(data);
+  lines = data.split('\n');
+  row = lines[lines.length - 3].split(/\s+/);
+  ra = hr2rad(parseFloat(row[2])
+	      + parseFloat(row[3]) / 60
+	      + parseFloat(row[4]) / 3600);
+  if (row[5][0] == '-') {
+    sgn = -1;
+  } else {
+    sgn = 1;
+  }
+  dec = deg2rad(parseFloat(row[5])
+		+ sgn * parseFloat(row[6]) / 60
+		+ sgn * parseFloat(row[7]) / 3600);
+  return {ra: ra, dec: dec};
+}
+
+function addSun(data) {
+  // data is a result from the IMCCE's ephemcc service
+  c = processIMCCE(data);
+  sun = generateData('Sun', c.ra, c.dec);
+  daylight = sun.alt.map(function(x){return x>0;});
+  twilight = sun.alt.map(function(x){return x>-0.314;});  // -18 deg = -0.314
+  sunset = ct[sun.alt.findIndex(function(e, i, a){return (ct[i]<0)&&(e<0);})];
+  sunrise = ct[sun.alt.findIndex(function(e, i, a){return (ct[i]<0)&&(e>0);})];
+  data = [
+    { type: 'box', x: [-12, sunset], y: [-90, 90] },
+    { type: 'box', x: [sunrise, 12], y: [-90, 90] }
+  ];
+  Plotly.addTraces('plot-window', data);
+}
 
 function generateData(name, ra, dec) {
   // ra, dec in radians
@@ -19,7 +68,7 @@ function generateData(name, ra, dec) {
   var lon = 0.0;  // rad
   var lst0 = ct2lst(date, lon);  // rad
 
-  var ct = [0];
+  var ct = [-Math.PI];
   for (var i=1; i<361; i++) {
     ct.push(ct[i-1] + 2 * Math.PI / 360);  // rad
   };
@@ -41,6 +90,9 @@ function plotData() {
   var data = [];
   
   for (var i=0; i<comets.length; i++) {
+    if (comets[i].startsWith('#')) {
+      continue;
+    }
     var row = comets[i].split(',');
     console.log(row);
     if (row.length != 3) {
@@ -53,4 +105,5 @@ function plotData() {
   }
 
   Plotly.newPlot('plot-window', data, layout);
+  getIMCCE('sun', 'p', $('#date').val(), addSun);
 }
