9336e1398441375c23bf758093fc5626bdecf18e
Michael S. P. Kelley <msk@astro.umd.edu>
2016-09-20 10:19:57 -0400

    Display more info from Miriade.

--- elevation.js
+++ elevation.js
@@ -4,7 +4,7 @@ $(document).ready(
     $('#elevation-load-button').click(loadTargets);
     $('#elevation-reset-button').click(clearTargets);
     $('.elevation-observatory').click(setLocation);
-    $('#elevation-date').on('input', updatePlot);
+    $('#elevation-date').on('change', updatePlot);
     $('#elevation-update-location-button').click(updatePlot);
     $('#target-set-load').click(loadTargetSet);
   }
@@ -54,34 +54,63 @@ function getIMCCE(name, type, done) {
 }
 
 function processIMCCE(data) {
+  /* Example IMCCE data:
+# Flag: 1
+# Ticket: 1474378574835
+# Solar system object ephemeris by IMCCE
+# Comet: P/Encke  (2P)
+# Source: numerical integration
+# Diameter (km): 0.00
+# CEU (arcsec): 0.00000000E+00
+# CEU rate (arcsec/d): 0.24566182E+07
+# Planetary theory INPOP13C
+# Astrometric J2000 coordinates
+# Frame center: geocenter
+# Relativistic perturbations, coordinate system 0
+# Equatorial coordinates (RA, DEC)
+#         Date UTC              R.A            Dec.          Distance     V.Mag   Phase   Elong.  muRAcosDE     muDE      Dist_dot
+#             h  m  s       h  m  s         o  '  "            AU                   o        o      "/min      "/min       km/s
+  2016-09-14T00:00:00.00    0 47  8.02777 +16  5  3.5421    1.629841562   17.60   10.46   152.44 -0.8100E+00 -0.1066E+00  -24.79021
+*/
   var lines = data.split('\n');
-  var name = lines[3].substr(2);
 
-  var m = name.match(/Asteroid: (.+)/);
+  var target = {};
+  target.name = lines[3].substr(2);
+
+  var m = target.name.match(/Asteroid: (.+)/);
   if (m !== null) {
-    name = m[1];
+    target.name = m[1];
   }
 
-  var m = name.match(/Comet:.*\((.+)\)/);
+  var m = target.name.match(/Comet:.*\((.+)\)/);
   if (m !== null) {
-    name = m[1];
+    target.name = m[1];
   }
   
-  var row = lines[lines.length - 3].split(/\s+/);
-  var ra = hr2rad(parseFloat(row[2])
-		  + parseFloat(row[3]) / 60
-		  + parseFloat(row[4]) / 3600);
+  var eph = lines[lines.length - 3].split(/\s+/);
+  target.ra = hr2rad(parseFloat(eph[2])
+		     + parseFloat(eph[3]) / 60
+		     + parseFloat(eph[4]) / 3600);
 
-  var sgn = -1 ? (row[5][0] == '-') : 1;
-  var dec = deg2rad(parseFloat(row[5])
-		    + sgn * parseFloat(row[6]) / 60
-		    + sgn * parseFloat(row[7]) / 3600);
+  var sgn = -1 ? (eph[5][0] == '-') : 1;
+  target.dec = deg2rad(parseFloat(eph[5])
+		       + sgn * parseFloat(eph[6]) / 60
+		       + sgn * parseFloat(eph[7]) / 3600);
+
+  target.delta = parseFloat(eph[8]);
+  target.mv = parseFloat(eph[9]);
+  target.phase = parseFloat(eph[10]);
+  target.elong = parseFloat(eph[11]);
+  target.motion = Math.sqrt(parseFloat(eph[12])**2 + parseFloat(eph[13])**2) * 60;
+  target.ddot = parseFloat(eph[14]);
+
   if (DEBUG) {
     console.log(data);
-    console.log(row);
-    console.log('RA, Dec:', ra, dec);
+    console.log(eph);
+    console.log(target);
   }
-  return {name: name, ra: ra, dec: dec};
+  
+  return target;
 }
 
 function addSun(coords) {
@@ -179,7 +208,7 @@ function updatePlot(e) {
     var targets = $('.elevation-target');
     if (targets.length > 0) {
       var coords = targets.map(function(i, x) {
-	return $(x).data('coords');
+	return $(x).data('target');
       });
       clearTargets();
       for (var i=0; i<coords.length; i++) {
@@ -217,25 +246,36 @@ function loadTargets() {
   }
 }
 
-function newTarget(coords) {
-  var targetControl = $('#elevation-target-control');
-  var input = $('<input class="elevation-target" type="checkbox" checked="checked">');
-  input.data('coords', coords);
-  input.change(toggleLine);
-  targetControl.append(input);
-  targetControl.append('<label>'
-		       + coords.name /* + ' ('
-		       + Math.round(rad2hr(coords.ra)) + ', '
-		       + Math.round(rad2deg(coords.dec)) + ')' */
-		       + '</label><br>');
-  plotTarget(coords);
+function newTarget(target) {
+  var tbody = $('#elevation-target-table tbody');
+  var row = $('<tr>');
+
+  row.append($('<td>').append(target.name));
+  row.append($('<td>').append(rad2hr(target.ra).toFixed(1)));
+  row.append($('<td>').append(rad2deg(target.dec).toFixed(1)));
+
+  if ('mv' in target) {
+    row.append($('<td>').append(target.mv.toFixed(1)));
+    row.append($('<td>').append(target.delta.toFixed(2)));
+    row.append($('<td>').append(target.ddot.toFixed(1)));
+    row.append($('<td>').append(target.phase.toFixed(0)));
+    row.append($('<td>').append(target.elong.toFixed(0)));
+    row.append($('<td>').append(target.motion.toFixed(0)));
+  } else {
+    for (var i=0; i<6; i++) {
+      row.append($('<td>'));
+    }
+  }
+
+  tbody.append(row);
+  plotTarget(target);
 }
 
-function plotTarget(coords) {
-  console.log(coords);
-  var altaz = generateAltAz(coords);
+function plotTarget(target) {
+  console.log(target);
+  var altaz = generateAltAz(target);
   var data = {
-    name: coords.name,
+    name: target.name,
     x: altaz.ct,
     y: altaz.alt,
     type: 'scatter',
@@ -252,18 +292,5 @@ function clearTargets() {
     traces.push(i);
   }
   Plotly.deleteTraces('elevation-plot', traces);
-  $('#elevation-target-control').html('');
-}
-
-function toggleLine(e) {
-  var plot = $('#elevation-plot')[0];
-  var coords = $(e.target).data('coords');
-  var traces = plot.data.map(function(x){return x.name;});
-  var i = traces.indexOf(coords.name);
-  console.log(i);
-  if (i >= 0) {
-    Plotly.deleteTraces('elevation-plot', traces.indexOf(coords.name));
-  } else {
-    plotTarget(coords);
-  }
+  $('#elevation-target-table tbody').html('');
 }
